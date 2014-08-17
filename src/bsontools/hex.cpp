92abfe3c31f95c8ca24ee2417846dbf8ade9403c
dwight <dwight@10gen.com>
2014-08-17 10:41:37 -0400

    -v option

--- src/bsontools/hex.cpp
+++ src/bsontools/hex.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 
     if (c.help()) {
         cout << "Hex dump utility. ";
-        cout << "Input from stdin will output in hexadecimal.\n" << endl;
+        cout << "Input from stdin, output in hexadecimal.\n" << endl;
         cout << "This utility has a tiny amount of BSON awareness.  It will output (in hex)\n";
         cout << "the total BSON object size, then on subsequent lines the document's content in hex.\n";
         cout << "Documents are separating by a blank line.  If non-bson data is encountered, it will\n";
@@ -22,10 +22,12 @@ int main(int argc, char* argv[])
         cout << "Options:\n";
         cout << "  -h    help\n";
         cout << "  -raw  raw hex dump with no 'bson awareness'" << endl;
+        cout << "  -v    verbose\n" << endl;
         return 0;
     }
 
     bool raw = c.got("raw");
+    bool verbose = c.got("v");
 
     binaryStdIn();
     
@@ -36,6 +38,7 @@ int main(int argc, char* argv[])
     int j = 0;
     char hist[4];
     int state = 4;
+    string txt;
     while ( 1 ) {
         cin.read(&ch, 1);
         if (cin.eof())
@@ -44,11 +47,20 @@ int main(int argc, char* argv[])
         state--;
         string s = _bson::toHex(&ch, 1);
         cout << s << ' ';
+        if( verbose ) {
+            if( ch == 0 )
+                txt += '.';
+            else
+                txt += (ch >= 32 ? ch : '.');
+        }
+
         if (state == 0) {
+            txt.clear();
             int sz = _bson::readInt(hist);
             state = sz;
-            //cout << " (" << sz << ")";
             if (!raw) {
+                if( verbose )
+                    cout << "                                        " << sz;
                 i = 0;
                 cout << endl;
                 continue;
@@ -57,16 +69,26 @@ int main(int argc, char* argv[])
         else if (state == 4) {
             j = 0; 
             if (!raw) {
+                if( !txt.empty() ) {
+                    for( long k = (i%16)+1; k < 16; k++ ) cout << "   ";
+                    cout << "    " << txt; txt.clear();
+                }
                 i = 0;
                 cin.peek();
+                cout << '\n';
                 if (!cin.eof() )
-                    cout << "\n\n";
+                    cout << "\n";
+                txt.empty();
                 continue;
             }
         }
         if (++i % 8 == 0) {
-            if (  i % 16 == 0 )
+            if (  i % 16 == 0 ) {
+                if( !txt.empty() ) {
+                    cout << "    " << txt; txt.clear();
+                }
                 cout << '\n';
+            }
         }
     }
 
